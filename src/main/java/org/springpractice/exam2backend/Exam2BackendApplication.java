968beae01f3180374d4cd4cce6dfa00d139c708e
Xen-alpha <hky7548@naver.com>
2025-02-26 09:20:25 +0900

    [Init] First commit

---
+++ src/main/java/org/springpractice/exam2backend/Exam2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.springpractice.exam2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam2BackendApplication.class, args);
+    }
+
+}
